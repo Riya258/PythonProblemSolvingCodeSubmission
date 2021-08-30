260d5132e9b04186fb994ab61c5487a9458c4123
Riya258 <89601021+Riya258@users.noreply.github.com>
2021-08-30 11:31:24 +0530

    Add files via upload

---
+++ Ps0.py
@@ -0,0 +1,7 @@
+# Problem Set 0
+# Name: Riya
+# Time Spent: 2:45
+
+date= input("Enter your date of birth:") #declare a variable named 'date' for enter your date of birth. 
+name= input("\nEnter your last name:") #declare a variable named 'name' for enter your last name. 
+print(name+ " " +date) #Prints out the user’s last name and date of birth, in the given order. 
